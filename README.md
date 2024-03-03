3d6f996bf989216d4f923fc9bfb22b447c028508
Asmin Silwal <asmin.076bei040@pcampus.edu.np>
2024-03-03 09:06:06 +0545

    Checksum added

---
+++ README.md
@@ -0,0 +1,4 @@
+## GeophoneReading
+
+This code is used to read the analog signals from Geophone sensor using
+ADS1256 adc with Adafruit ESP32 Feather microcontroller. 
---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+#include "../lib/ADS1256/ADS1256.h"
+
+//nodeID is used for identification of data source in the server
+#define NODEID 00
+
+float clockMHZ = 7.68; // crystal frequency used on ADS1256
+float vRef = 2.5; // voltage reference
+
+float geoX,geoY,geoZ;
+ADS1256 *adc;
+
+typedef struct {
+    int node_ID;
+    int checksum;
+    double_t data[3];
+}geophoneDATA;
+
+typedef union{
+    double_t data;
+    int bin;
+} converter;
+
+geophoneDATA sensorDATA;
+void setup()
+{
+    adc = new ADS1256(clockMHZ,vRef,false);
+    Serial.begin(115200);
+    Serial.println("Starting ADC");
+
+    sensorDATA.node_ID=NODEID;
+    sensorDATA.checksum=0;
+    for(int i=0; i<3; i++){
+          sensorDATA.data[i]=0;
+    }
+
+    adc->begin(ADS1256_DRATE_1000SPS,ADS1256_GAIN_1,false);
+    adc->offsetCalibration();
+
+    Serial.println("ADC Started");
+    adc->setChannel(0,1);
+}
+int calculateChecksum(geophoneDATA geoReadings);
+void serializeStruct(Stream &stream, const void *geoReadings, size_t size);
+void loop()
+{
+    adc->waitDRDY(); // wait for DRDY to go low before next register read
+    sensorDATA.data[0]=adc->readCurrentChannel(); // read as voltage according to gain and vref
+    adc->waitDRDY();
+    adc->setChannel(2,3);
+    sensorDATA.data[1]=adc->readCurrentChannel();
+    adc->waitDRDY();
+    adc->setChannel(4,5);
+    sensorDATA.data[2]=adc->readCurrentChannel();
+
+    sensorDATA.checksum= calculateChecksum(sensorDATA);
+    serializeStruct(Serial, &sensorDATA,sizeof(sensorDATA));
+}
+
+int calculateChecksum(geophoneDATA geoReadings){
+    int checksum=0;
+    converter conv;
+    for (int i=0;i<3; i++){
+        conv.data=geoReadings.data[i];
+        checksum+=conv.bin;
+    }
+    if (checksum=0) return -checksum;
+    return checksum;
+}
+void serializeStruct(Stream &stream, const void *geoReadings, size_t size) {
+    // sof marker 0x7E
+    uint8_t sof = 0x7E;
+    stream.write(sof);
+    // length of the packet
+    uint8_t length = size + 2;
+    stream.write(length);
+    const uint8_t *ptr = (const uint8_t *)geoReadings;
+    for (size_t i = 0; i < size; i++) {
+        stream.write(ptr[i]);
+    }
+};
